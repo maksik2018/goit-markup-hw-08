0d0bb9d8bed941378afc21f0a22c48aac91fca3a
Maksim <gashenko100@ukr.net>
2021-03-28 14:10:15 +0300

    end

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,33 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-modal-open]'),
+//       closeModalBtn: document.querySelector('[data-modal-close]'),
+//       modal: document.querySelector('[data-modal]'),
+//     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle('backdrop__is-hidden');
+//     function toggleModal() {
+//       refs.modal.classList.toggle('backdrop__is-hidden');
 
-     }
-  })();
+//      }
+//   })();
+
+// модальное окно с запрещенным скроллом
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop__is-hidden");
+    refs.body.classList.toggle("is-modal-open");
+  }
+})();
